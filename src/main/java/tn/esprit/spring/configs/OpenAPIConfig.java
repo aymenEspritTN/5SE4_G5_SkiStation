689ca50e6bbeac6494c17ff877e67312b9e2bd1b
aymen <aymen.khiari@esprit.tn>
2024-11-13 13:09:41 +0100

    swagger2

--- src/main/java/tn/esprit/spring/configs/OpenAPIConfig.java
+++ src/main/java/tn/esprit/spring/configs/OpenAPIConfig.java
@@ -3,7 +3,7 @@ package tn.esprit.spring.configs;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,6 +38,4 @@ public class OpenAPIConfig {
                 .pathsToExclude("**")
                 .build();
     }
-
-
 }
